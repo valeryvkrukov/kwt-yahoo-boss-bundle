94aa8cfdb029f404442607e72345a6cc0cdabd84
valeryvkrukov <valery.v.krukov@gmail.com>
2014-01-11 08:31:05 +0200

    added check for optional params

--- Service/BOSS.php
+++ Service/BOSS.php
@@ -7,7 +7,7 @@ use KWT\Yahoo\BossBundle\Core\MethodHMACSHA1;
 use KWT\Yahoo\BossBundle\Exception\YahooBossApiException;
 
 class BOSS{
-	const YAHOO_BOSS_URL='http://yboss.yahooapis.com/ysearch/news,web,images';
+	private $_api_url='http://yboss.yahooapis.com/ysearch/news,web,images';
 	private $_consumer=null;
 	private $_url=null;
 	private $_headers=null;
@@ -16,13 +16,21 @@ class BOSS{
 		$this->_consumer=new \stdClass();
 		$this->_consumer->key=$container->getParameter('kwt_yahoo_boss_key');
 		$this->_consumer->secret=$container->getParameter('kwt_yahoo_boss_secret');
-		$this->_consumer->callback_url=$container->getParameter('kwt_yahoo_boss_callback');
+		if($container->hasParameter('kwt_yahoo_boss_api_url')){
+			$this->_api_url=$container->getParameter('kwt_yahoo_boss_api_url');
+		}
+		if($container->hasParameter('kwt_yahoo_boss_callback')){
+			$this->_consumer->callback_url=$container->getParameter('kwt_yahoo_boss_callback');
+		}
 	}
 	
-	public function callService(array $args){		
-		$request=Request::from_consumer_and_token($this->_consumer,null,'GET',self::YAHOO_BOSS_URL,$args);
+	public function callService(array $args){
+		if(!isset($args['format'])){
+			$args['format']='json';
+		}
+		$request=Request::from_consumer_and_token($this->_consumer,null,'GET',$this->_api_url,$args);
 		$request->sign_request(new MethodHMACSHA1(),$this->_consumer,null);
-		$this->_url=sprintf('%s?%s',self::YAHOO_BOSS_URL,Util::build_http_query($args));
+		$this->_url=sprintf('%s?%s',$this->_api_url,Util::build_http_query($args));
 		$this->_headers=array($request->to_header());
 		$this->getResult($request);
 	}
