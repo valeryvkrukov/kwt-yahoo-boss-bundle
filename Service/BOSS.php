9a2362c55f942edf81187436ebe636aa2f265cd9
valeryvkrukov <valery.v.krukov@gmail.com>
2014-01-13 01:12:21 +0200

    adapted for plafecinder

--- Service/BOSS.php
+++ Service/BOSS.php
@@ -7,7 +7,7 @@ use KWT\Yahoo\BossBundle\Core\MethodHMACSHA1;
 use KWT\Yahoo\BossBundle\Exception\YahooBossApiException;
 
 class BOSS{
-	private $_api_url='http://yboss.yahooapis.com/ysearch/news,web,images';
+	private $_api_url='http://yboss.yahooapis.com/geo/placefinder';
 	private $_consumer=null;
 	private $_url=null;
 	private $_headers=null;
@@ -16,21 +16,15 @@ class BOSS{
 		$this->_consumer=new \stdClass();
 		$this->_consumer->key=$container->getParameter('kwt_yahoo_boss_key');
 		$this->_consumer->secret=$container->getParameter('kwt_yahoo_boss_secret');
-		if($container->hasParameter('kwt_yahoo_boss_api_url')){
-			$this->_api_url=$container->getParameter('kwt_yahoo_boss_api_url');
-		}
 		if($container->hasParameter('kwt_yahoo_boss_callback')){
 			$this->_consumer->callback_url=$container->getParameter('kwt_yahoo_boss_callback');
 		}
 	}
 	
-	public function callService(array $args){
-		if(!isset($args['format'])){
-			$args['format']='json';
-		}
-		$request=Request::from_consumer_and_token($this->_consumer,null,'GET',$this->_api_url,$args);
+	public function callService($query){
+		$request=Request::from_consumer_and_token($this->_consumer,null,'GET',$this->_api_url,array('q'=>$query));
 		$request->sign_request(new MethodHMACSHA1(),$this->_consumer,null);
-		$this->_url=sprintf('%s?%s',$this->_api_url,Util::build_http_query($args));
+		$this->_url=sprintf('%s?%s',$this->_api_url,Util::build_http_query(array('q'=>$query)));
 		$this->_headers=array($request->to_header());
 		return $this->getResult($request);
 	}
@@ -44,11 +38,19 @@ class BOSS{
 		if(($response=curl_exec($curl))==false){
 			throw new YahooBossApiException('Connection error');
 		}
-		$response=json_decode($response);
+		/*$response=$this->xmlToJson($response);
 		if(isset($response->error)){
 			throw new YahooBossApiException($response->error->description.': '.$response->error->detail);
-		}
+		}*/
 		return $response;
+	}
+	
+	protected function xmlToJson($xml){
+		$contents=str_replace(array("\n","\r","\t"),'',$xml);
+		$contents=trim(str_replace('"',"'",$contents));
+		$sx=simplexml_load_string($contents);
+		$json=json_encode($sx);
+		return $json;
 	}
 	
 }
